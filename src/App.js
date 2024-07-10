5af92731f1bc0de70375550d64f5b2762962bff2
Amir Rahi <amirrahi29@gmail.com>
2024-07-10 22:02:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{marginLeft:300,marginTop:200}}>Hello amir rahi, this application is deploying on AWS</div>
+  )
+}
+
+export default App
