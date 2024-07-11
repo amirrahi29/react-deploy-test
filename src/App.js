5a20826776a0d41dadaf4e407916b7c6f148edda
Amir Rahi <amirrahi29@gmail.com>
2024-07-11 13:18:31 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div style={{marginLeft:300,marginTop:200}}>Hello amir rahi, this application is deploying on AWS</div>
+    <div style={{marginLeft:300,marginTop:200}}>
+      <marquee>Hello amir rahi, this application is deploying on AWS   tttttt</marquee>
+    </div>
   )
 }
 
